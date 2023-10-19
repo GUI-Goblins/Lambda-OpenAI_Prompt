087f847353abd6390d9498823d1c61d8467f91dc
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-19 13:09:58 -0700

    updated lambda 3 to testFunction

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ exports.handler = async (event) => {
       const openAi = openAiResponse.data.choices[0].message.content;
       console.log('Here is our open ai response before being parsed:', openAi);
       const params = {
-        FunctionName: 'characterRoll',
+        FunctionName: 'testFunction',
         InvocationType: 'RequestResponse',
         Payload: JSON.stringify(openAi),
       };
